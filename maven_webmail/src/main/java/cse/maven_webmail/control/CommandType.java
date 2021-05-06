2f793b4c1afbed3dc57398c6a0cc9a97ef2caf82
김예진 <zinyizhen8@gmail.com>
2021-05-06 17:14:59 +0900

    remove warning CommandType.java
    add private constructor , reorder the modifiers

--- maven_webmail/src/main/java/cse/maven_webmail/control/CommandType.java
+++ maven_webmail/src/main/java/cse/maven_webmail/control/CommandType.java
@@ -6,20 +6,23 @@
 package cse.maven_webmail.control;
 
 public class CommandType {
-
-    public static final int READ_MENU = 1;
-    public final static int WRITE_MENU = 2;
-
-    public final static int ADD_USER_MENU = 3;
-    public final static int DELETE_USER_MENU = 4;
-
-    public final static int SEND_MAIL_COMMAND = 21;
-    public final static int DELETE_MAIL_COMMAND = 41;
-    public final static int DOWNLOAD_COMMAND = 51;
     
-    public final static int ADD_USER_COMMAND = 61;
-    public final static int DELETE_USER_COMMAND = 62;
+    private CommandType(){
+        // not called private constructor
+    }
+    public static final int READ_MENU = 1;
+    public static final int WRITE_MENU = 2;
 
-    public final static int LOGIN = 91;
-    public final static int LOGOUT = 92;
+    public static final int ADD_USER_MENU = 3;
+    public static final int DELETE_USER_MENU = 4;
+
+    public static final int SEND_MAIL_COMMAND = 21;
+    public static final int DELETE_MAIL_COMMAND = 41;
+    public static final int DOWNLOAD_COMMAND = 51;
+    
+    public static final int ADD_USER_COMMAND = 61;
+    public static final int DELETE_USER_COMMAND = 62;
+
+    public static final int LOGIN = 91;
+    public static final int LOGOUT = 92;
 }
